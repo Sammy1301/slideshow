571691fb1451a098ee7da40a6627e9187e579ec3
Sammy1301 <72657003+Sammy1301@users.noreply.github.com>
2021-01-18 10:54:20 -0600

    18-01-2021
    Navbar fixes, Fixed transition bug in Services, responsive features added.
    arreglada la nav bar, arreglado bug de transicion en los servicios, responsividad.

--- dr terrero/vida.js
+++ dr terrero/vida.js
@@ -1,35 +1,32 @@
 function fecha(){
     var time = document.getElementById("fecha");
     var dat = new Date();
-    var months = ['Enero', 'Febrero', 'Marzo', 'Abril','Mayo', 'Junio', 'Julio', 'Agosto','Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-    time.innerText = months[dat.getMonth()]+ " " +dat.getFullYear();
+    time.innerText =dat.getFullYear();
 }
-// var pantalla = window.innerWidth;
-// collapse(pantalla);
-// function collapse(x){
-//     // var menu = ;
-//     console.log(document.getElementById("menu"));
-//     if(x<=768){
-//         document.getElementById("menu").style.display = "flex";
-//         document.getElementById("menu").style.flexDirection = "column";
-//     }
-// }
-
-
 //#region animacion de servicios
+$(document).ready(function() {
+
 window.addEventListener('scroll', servicios);
 function servicios(){
     var a = Math.round(window.scrollY);
-    if(a>=151 ){
-        document.getElementById("imgs").style.display = "flex";
+    if(a>=100 ){
+        $("#imgs").fadeIn(2000);
+        $("#imgs").css({"display":"flex"});
+        
     }
     if(a>=400){
-        document.getElementById("sono").style.display = "flex";
+        $("#cons").fadeIn(2000);
+        $("#cons").css({"display":"flex"});
     }
-    if(a>=578){
-        document.getElementById("cons").style.display = "flex";
+    if(a>=680){
+        $("#sono").fadeIn(2000);
+        $("#sono").css({"display":"flex"});
     }
-}
+}    
+})
+
+
+
 //#endregion
 function modal(){
     document.getElementById("modal").style.display = "block";
@@ -41,14 +38,3 @@ function qmodal(){
 }
 
 
-$(document).ready(function(){
-    $(".logo").on("click", function(){
-
-    // $("#menu").css({"display": "flex", 
-    // "flex-direction":"column", 
-    // "z-index":"999", 
-    // "margin-top":"130px", 
-    // "align-items":"center"});
-  $("body").effect("shake");
-  })
-  });
